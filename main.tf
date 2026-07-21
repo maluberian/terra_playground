7630acd3b53855173977e9665a0a59ca2b2d46f8
Dustin Clifford <dclifford@sitesoftllc.net>
2026-07-21 00:59:01 -0400

    using variables

--- main.tf
+++ main.tf
@@ -15,7 +15,7 @@ resource "docker_image" "nginx" {
 }
 
 resource "docker_container" "nginx" {
-  name  = "super-awesome-nginx"
+  name  = var.container_name
   image = docker_image.nginx.image_id
   ports {
     internal = 80
---
+++ variables.tf
@@ -0,0 +1,6 @@
+variable "container_name" {
+    description = "The name of the container to be created."
+    type        = string
+    default     = "less-awesome-nginx"
+  
+}
